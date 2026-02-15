672e6fcc07a63d5bbe21f38c35ee4bf7967e6a61
Dev-MuTTiNeeR <cemakkaya1312@gmail.com>
2026-02-15 16:12:24 -0300

    Small fixes

--- Controller/PokemonViewController.swift
+++ Controller/PokemonViewController.swift
@@ -17,7 +17,7 @@ class PokemonViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        manager.delegate = self
+        //manager.delegate = self
         
     }
     
@@ -25,43 +25,42 @@ class PokemonViewController: UIViewController {
 
 // MARK: - PokemonManagerDelegate
 
-extension PokemonViewController: PokemonManagerDelegate {
-    func didUpdatePokemon(pokemon: PokemonData) {
-        
-        // THIS IS VERY CRITICAL!
-        // Internet operations are performed in the background (Background Thread).
-        // However, changing the text (label) on the screen must be done in the main thread.
-        // Bunu yapmazsanız uygulama çökebilir veya çok yavaşlar.
-        
-        DispatchQueue.main.async {            
-            self.nameLabel.text = pokemon.name.capitalized // Capitalizes the first letter of the name
-        }
-        
-        DispatchQueue.main.async {
-            self.statLabel.text = String(pokemon.stats[1].base_stat)
-        }
-        
-        // 2. Now it's the IMAGE's turn (String -> URL -> Data -> Image)
-        // Convert the link to a secure URL
-        if let url = URL(string: pokemon.sprites.front_default) {
-            
-            // Downloading the image requires internet access, so we're doing it in the BACKGROUND (global).
-            DispatchQueue.global().async {
-                
-                // Try downloading the data (image) from the link
-                if let data = try? Data(contentsOf: url) {
-                    
-                    // Displaying the downloaded data is a VISUAL task, so we're returning to the MAIN LINE.
-                    DispatchQueue.main.async {
-                        self.pokemonImage.image = UIImage(data: data)
-                    }
-                }
-            }
-        }
-    }
-    
-    func didFailWithError(error: any Error) {
-        print("Error: \(error)")
-    }
-}
-
+//extension PokemonViewController: PokemonManagerDelegate {
+//    func didUpdatePokemon(pokemon: PokemonData) {
+//        
+//        // THIS IS VERY CRITICAL!
+//        // Internet operations are performed in the background (Background Thread).
+//        // However, changing the text (label) on the screen must be done in the main thread.
+//        
+//        DispatchQueue.main.async {            
+//            self.nameLabel.text = pokemon.name.capitalized // Capitalizes the first letter of the name
+//        }
+//        
+//        DispatchQueue.main.async {
+//            self.statLabel.text = String(pokemon.stats[1].base_stat)
+//        }
+//        
+//        // 2. Now it's the IMAGE's turn (String -> URL -> Data -> Image)
+//        // Convert the link to a secure URL
+//        if let url = URL(string: pokemon.sprites.front_default) {
+//            
+//            // Downloading the image requires internet access, so we're doing it in the BACKGROUND (global).
+//            DispatchQueue.global().async {
+//                
+//                // Try downloading the data (image) from the link
+//                if let data = try? Data(contentsOf: url) {
+//                    
+//                    // Displaying the downloaded data is a VISUAL task, so we're returning to the MAIN LINE.
+//                    DispatchQueue.main.async {
+//                        self.pokemonImage.image = UIImage(data: data)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    
+//    func didFailWithError(error: any Error) {
+//        print("Error: \(error)")
+//    }
+//}
+//
